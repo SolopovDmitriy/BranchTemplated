05b1c1490df54599a80c6ba287e9d54f76e07e2e
Dmitriy Solopov <div.solopov@gmail.com>
2020-03-26 00:23:12 +0200

    separate_templated

--- BranchTemplated/main.cpp
+++ BranchTemplated/main.cpp
@@ -1,30 +1,13 @@
-#include<iostream>
-using namespace std;
-
-const int ROWS = 10;
-const int COLS = 10;
-
-template<typename T>
-void FillRand(T arr[], const int n);
-template<typename T>
-void FillRand(T arr[ROWS][COLS], const int ROWS, const int COLS);
-
-template<typename T>
-void Print(T arr[], const int n);
-template<typename T>
-void Print(T arr[ROWS][COLS], const int ROWS, const int COLS);
-
-template<typename T>
-void Sort(T arr[], const int n);
-template<typename T>
-void Sort(T arr[ROWS][COLS], const int ROWS, const int COLS);
+#include"Header.h"
+#include"fillrand.cpp"
+#include"print.cpp"
+#include"sort.cpp"
 
 //Git Branches
 
 void main()
 {
 
-	cout << "Hello world1" << endl;
 	const int n = 5;
 	int arr[n];
 	FillRand(arr, n);
@@ -45,100 +28,4 @@ void main()
 	Print(A, ROWS, COLS);
 	Sort(A, ROWS, COLS);
 	Print(A, ROWS, COLS);
-}
-template<typename T>
-void FillRand(T arr[], const int n)
-{
-	//cout << typeid(arr).name() << endl;
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 100;
-		arr[i] /= 10;
-	}
-}
-
-template<typename T>
-void FillRand(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand() % 100;
-		}
-	}
-}
-template<typename T>
-void Print(T arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
-template<typename T>
-void Print(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
-template<typename T>
-void Sort(T arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[j] < arr[i])
-			{
-				T buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
-			}
-		}
-	}
-}
-
-template<typename T>
-void Sort(T arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	int iterations = 0;
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			for (int k = i; k < ROWS; k++)
-			{
-				/*int l;
-				if (k == i)
-				{
-				l = j + 1;
-				}
-				else
-				{
-				l = 0;
-				}*/
-				// condition ? value1 : value2;
-				for (int l = k == i ? j + 1 : 0; l < COLS; l++)
-				{
-					if (arr[k][l] < arr[i][j])
-					{
-						T buffer = arr[i][j];
-						arr[i][j] = arr[k][l];
-						arr[k][l] = buffer;
-					}
-					iterations++;
-				}
-			}
-		}
-	}
-	cout << "Number of iterations: " << iterations << endl;
 }
---
+++ BranchTemplated/sort.cpp
@@ -0,0 +1,54 @@
+#include"Header.h"
+
+template<typename T>
+void Sort(T arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				T buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+
+template<typename T>
+void Sort(T arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	int iterations = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			for (int k = i; k < ROWS; k++)
+			{
+				/*int l;
+				if (k == i)
+				{
+				l = j + 1;
+				}
+				else
+				{
+				l = 0;
+				}*/
+				// condition ? value1 : value2;
+				for (int l = k == i ? j + 1 : 0; l < COLS; l++)
+				{
+					if (arr[k][l] < arr[i][j])
+					{
+						T buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+					}
+					iterations++;
+				}
+			}
+		}
+	}
+	cout << "Number of iterations: " << iterations << endl;
+}
